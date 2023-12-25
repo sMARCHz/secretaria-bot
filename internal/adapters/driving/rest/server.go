5fd5e70b88ef1431a53d3fa61a0bf4112f326eed
Nattanon Chansamakr <mart_nattanon@hotmail.com>
2023-12-25 19:27:41 +0700

    Typo

--- internal/adapters/driving/rest/server.go
+++ internal/adapters/driving/rest/server.go
@@ -19,13 +19,13 @@ import (
 )
 
 func Start(config config.Configuration) {
-	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
 	server := &http.Server{
 		Addr:    fmt.Sprintf(":%v", config.App.Port),
 		Handler: buildHandler(config),
 	}
 
-	// Start server
+	// Starting server
 	go func() {
 		logger.Infof("Listening and serving HTTP on :%v", config.App.Port)
 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
@@ -33,11 +33,11 @@ func Start(config config.Configuration) {
 		}
 	}()
 
-	// Shutdown server
+	// Shutting down
 	<-ctx.Done()
-	stop()
+	cancel()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := server.Shutdown(ctx); err != nil {
 		logger.Fatal("Server forced to shutdown: ", err)
